d68edec1774e3e701711f1e60f6095d1179359de
winsofttutorials <winsofttutorials@gmail.com>
2023-06-12 17:44:59 +0100

    'styling'

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from "./Components/Hero/Hero";
 
 import Products from "./Components/Products/Products";
 import Services from "./Components/Services/Services";
-// import Subscribe from "./Components/Subscribe/Subscribe";
+
 import Testimonial from "./Components/Testimonial/Testimonial";
 import Footer from "./Components/Footer/Footer";
 
@@ -19,7 +19,6 @@ const App = () => {
         <Services />
         <Products />
         <Testimonial />
-
         <Footer />
       </div>
     </>
